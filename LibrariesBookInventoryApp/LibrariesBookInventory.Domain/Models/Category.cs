040b0c88ea7676c5b827f8de1de04395d546b578
Andriy Savriy <andriysavriy@gmail.com>
2023-07-03 06:08:33 +0300

    Books in Category marked as virtual

--- LibrariesBookInventoryApp/LibrariesBookInventory.Domain/Models/Category.cs
+++ LibrariesBookInventoryApp/LibrariesBookInventory.Domain/Models/Category.cs
@@ -8,6 +8,6 @@ namespace LibrariesBookInventory.Domain.Models
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public ICollection<Book> Books { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
     }
 }
